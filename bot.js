1358000de7324f9d2f0cb3abd663ac0809bcb7db
alekssawa <38007759+alekssawa@users.noreply.github.com>
2019-03-16 14:54:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,8 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
+    if (message.content === 'pig') {
+    	message.reply('pog');
   	}
 });
 
